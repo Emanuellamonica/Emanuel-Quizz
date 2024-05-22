3da55c11c4a13f4f9d0e19575ef74449d6249bae
Emanuellamonica <167205977+Emanuellamonica@users.noreply.github.com>
2024-05-22 12:07:06 -0300

    Jogo Navinha

---
+++ EstudosJogos/Assets/Navinha/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instancia;
+
+    [Header("Gerador de Alans")]
+    public GameObject objetoAlan;
+    public Transform[] geradoresAlan;
+    public float taxaAlan;
+
+    private void Awake()
+    {
+        instancia = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GerarAlan());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator GerarAlan()
+    {
+        int rnd = Random.Range(0,geradoresAlan.Length);
+        Instantiate(objetoAlan, geradoresAlan[rnd].position, Quaternion.identity);
+        yield return new WaitForSeconds(taxaAlan);
+        StartCoroutine(GerarAlan());
+    }
+}
